b031d73368624cfcf9823533fc470df4d8c65884
iskander-d <drozenko21@gmail.com>
2023-12-03 14:27:55 +0400

    added.steps

--- src/test/java/tests/RegistrationPageObject.java
+++ src/test/java/tests/RegistrationPageObject.java
@@ -58,28 +58,54 @@ public class RegistrationPageObject extends TextBoxTests {
     @Test
     @Tag("simple")
     void minimalData() {
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName("Alexander")
-                .setLastName("Drozenko")
-                .setUserEmail("drozenko21@gmail.com")
-                .setGender("Male")
-                .setUserNumber("0506488515")
-                .pressSubmit();
-        registrationPage.checkResult("Student Name", "Alexander Drozenko")
-                .checkResult("Student Email", "drozenko21@gmail.com")
-                .checkResult("Gender", "Male")
-                .checkResult("Mobile", "0506488515");
+        step("Открываем страницу с формой", () -> {
+
+            registrationPage.openPage();
+        });
+        step("Убираем рекламу", () -> {
+
+            registrationPage.removeBanner();
+        });
+        step("Заполняем обязательные поля", () -> {
+            registrationPage
+                    .setFirstName("Alexander")
+                    .setLastName("Drozenko")
+                    .setUserEmail("drozenko21@gmail.com")
+                    .setGender("Male")
+                    .setUserNumber("0506488515")
+                    .pressSubmit();
+        });
+        step("Проверяем отображение введенных значений в таблице", () -> {
+            registrationPage.checkResult("Student Name", "Alexander Drozenko")
+                    .checkResult("Student Email", "drozenko21@gmail.com")
+                    .checkResult("Gender", "Male")
+                    .checkResult("Mobile", "0506488515");
+        });
 
     }
 
     @Test
     @Tag("simple")
     void negativeTest() {
-        registrationPage.openPage()
-                .removeBanner()
-                .pressSubmit()
-                .checkNotCompleteForm();
+
+        step("Открываем страницу с формой", () -> {
+
+            registrationPage.openPage();
+        });
+        step("Убираем рекламу", () -> {
+
+            registrationPage.removeBanner();
+        });
+        step("Подтверждаем заполнение полей данных кнопкой Submit", () -> {
+            registrationPage.pressSubmit();
+        });
+
+
+        step("Проверяем что обязательные поля выделены красным цветом", () -> {
+            registrationPage
+                    .checkNotCompleteForm();
+        });
+
 
     }
 
