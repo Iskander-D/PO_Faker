ee7f954a48a6a81814909e3106ed6534b762b34c
iskander-d <drozenko21@gmail.com>
2025-07-06 12:38:28 +0300

    add component

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,6 +1,11 @@
 package pages.components;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
         public void setDate(String day, String month, String year) {
@@ -10,5 +15,15 @@ public class CalendarComponent {
             $(".react-datepicker__day--" + formattedDay + ":not(.react-datepicker__day--outside-month)")
                     .click();
 
+//            private SelenideElement monthSelect = $(".react-datepicker__month-select"),
+//                    yearSelect = $(".react-datepicker__year-select");
+//
+//            private ElementsCollection daySelect = $$(".react-datepicker__day");
+//
+//            public void setDate(String month, String year, String day) {
+//                monthSelect.selectOption(month);
+//                yearSelect.selectOption(year);
+//                daySelect.findBy(text(day)).click();
+            }
 //            $(".react-datepicker__day.react-datepicker__day--0"+day+":not(.react-datepicker__day--outside-month)").click();
-        }}
+        }
